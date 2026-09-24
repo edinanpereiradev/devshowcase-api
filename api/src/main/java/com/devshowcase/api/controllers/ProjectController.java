c4ad68b4189f04824f5c126edc2d28791d8c43d8
Edinan Pereira <edinan93@gmail.com>
2026-09-24 12:46:04 -0300

    fix: remove duplicacao no ProjectController

--- api/src/main/java/com/devshowcase/api/controllers/ProjectController.java
+++ api/src/main/java/com/devshowcase/api/controllers/ProjectController.java
@@ -14,22 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-package com.devshowcase.api.controllers;
-
-import com.devshowcase.api.dtos.FeedbackRequestDTO;
-import com.devshowcase.api.dtos.FeedbackResponseDTO;
-import com.devshowcase.api.dtos.ProjectRequestDTO;
-import com.devshowcase.api.dtos.ProjectResponseDTO;
-import com.devshowcase.api.services.ProjectService;
-import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
 @RequestMapping("/api/projects")
 public class ProjectController {
 
@@ -39,7 +23,7 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
-    // POST /api/projects -> ENDPOINT QUE FALTAVA
+    // POST /api/projects
     @PostMapping
     public ResponseEntity<ProjectResponseDTO> create(@Valid @RequestBody ProjectRequestDTO dto) {
         ProjectResponseDTO responseDto = projectService.create(dto);
